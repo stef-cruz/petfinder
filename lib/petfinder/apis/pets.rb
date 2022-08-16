4475bbc2565a26f86e8666513c82dd3f10872ac0
stef-cruz <scruz@kitmanlabs.com>
2022-08-16 13:15:56 +0100

    Add new endpoint

--- lib/petfinder/apis/pets.rb
+++ lib/petfinder/apis/pets.rb
@@ -11,6 +11,13 @@ module Petfinder
         JSON.parse(response.body || {})['animals']
       end
 
+      def pet_by_id(id)
+        connect_to_api = connection
+        response = connect_to_api.get(id.to_s)
+        puts "Failed to connect to the API. Response status: #{response.status}." if response.status != 200
+        JSON.parse(response.body || {})['animal']
+      end
+
     end
   end
 end
